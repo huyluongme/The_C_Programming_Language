4f5e6b6414e4227e52dfdb8ffcabc621fa5d3c51
huyluongme <huyluongme.cs@gmail.com>
2025-06-22 23:37:14 +0700

    update 1_1_hello.c

--- 1_1_hello.c
+++ 1_1_hello.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>				// include information about standard
 
-int main()
-{
-	printf("hello, world\n");
-	return 0;
+int main()					// define a function called main, that received no argument values
+{						// statements of main are enclosed in braces
+	printf("hello, world\n");		// main calls library function printf to print this sequence of characters
+	return 0;				// \n represents the newline character
 }
