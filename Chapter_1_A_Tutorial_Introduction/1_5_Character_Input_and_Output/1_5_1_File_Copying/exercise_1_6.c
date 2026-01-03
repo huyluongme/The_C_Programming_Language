5c0a7d180c4033e82ce881a2a4796c40e0c8238f
Huy Luong Duc <huyluongme.cs@gmail.com>
2026-01-03 09:37:52 +0000

    add section 1_3, 1_4, 1_5, 1_5_1

---
+++ Chapter_1_A_Tutorial_Introduction/1_5_Character_Input_and_Output/1_5_1_File_Copying/exercise_1_6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Exercise 1-6. Verify that the expression getchar() != EOF is 0 or 1. */
+int main()
+{
+    int c;
+    
+    while((c = getchar()) != EOF)
+        printf("getchar() != EOF is: %d\n", c != EOF);
+
+    printf("getchar() != EOF is: %d\n", c != EOF);
+    return 0;
+}
